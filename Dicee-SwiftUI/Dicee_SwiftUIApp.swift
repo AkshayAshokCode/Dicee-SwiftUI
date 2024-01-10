7526fa0383cbffb8e5e2c8bb3bebdb1ea7433847
akshay ashok <akshayashokan1054@gmail.com>
2024-01-10 06:51:05 +0530

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Akshay Ashok on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
